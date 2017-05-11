61384414a64200a2172d065b86f30e795198b7c5
alissonjr <web.alisson@aticonsultoria.com>
2017-05-11 17:31:35 -0300

    criação dos membros

--- escalas-backend/server/modules/cultos/controller.js
+++ escalas-backend/server/modules/cultos/controller.js
@@ -27,7 +27,7 @@ export const createCulto = async (req, res) => {
         }
     ];
 
-    validation = validation.map((item) => validate(item.field, item.value, item.validation)).filter((item) => item);
+    validation = validation.map(item => validate(item.field, item.value, item.validation)).filter(item => item);
 
     if (validation.length > 0) {
         return res.status(400).json({ error: true, messages: validation });
@@ -44,22 +44,15 @@ export const createCulto = async (req, res) => {
 // --------------------------------------------------
 export const getAllCultos = async (req, res) => {
     try {
-        return res.status(200).json({ cultos: Culto.find({}).project({ period: 1, description: 1, duration: 1, updatedAt: 0, createdAt: 0, _id: 0, _v: 0 }) });
+        return res.status(200).json({ cultos: Culto.find({}) });
     } catch (e) {
         return res.status(e.status).json({ error: true, message: 'Nenhum culto encontrado' });
     }
 };
 // --------------------------------------------------
 export const getCulto = async (req, res) => {
-
-    const id_culto = req.params.id_culto;
-    const validation = validate(item.field, item.value, item.validation) !== false;
-
-    if(validation)
-        return res.status(400).json({ error: true, message: validation });
-
     try {
-        return res.status(200).json(await Culto.findById());
+        return res.status(200).json(await Culto.findById(req.params.id_culto));
     } catch (e) {
         return res.status(e.status).json({ error: true, message: 'Culto não encontrado' });
     }
